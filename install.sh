54ae93425511a9ae288352085428a492542ec13f
Abhiak99 <78052345+Abhiak99@users.noreply.github.com>
2022-01-16 02:19:59 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+if [[ "${EUID}" -ne 0 ]]; then
+  echo -e "[-] This script must be run as root"
+  sleep 2s
+  exit 1
+fi
+
+cp /usr/bin/python2 /usr/bin/python 
+cd ~
+wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
+rm get-pip.py
+python get-pip.py
+su
+cd ~
+wget https://bootstrap.pypa.io/get-pip.py -o get-pip3.py
+python3 get-pip3.py
+rm get-pip3.py

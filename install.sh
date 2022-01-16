678fb1bfdddd9dd69fce97e5b414762f52de28ff
Abhiak99 <78052345+Abhiak99@users.noreply.github.com>
2022-01-16 20:12:40 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,11 +7,34 @@ if [[ "${EUID}" -ne 0 ]]; then
   exit 1
 fi
 
-cp /usr/bin/python2 /usr/bin/python 
+echo "[*] Copying python2 as python"
+echo "[*] cp /usr/bin/python2 /usr/bin/python"
+cp /usr/bin/python2 /usr/bin/python
+echo "[*] Downloading get-pip.py for python2.7"
+echo "[*] wget https://bootstrap.pypa.io/pip/2.7/get-pip.py" 
 wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
+echo "[*] python get-pip.py"
 python get-pip.py
-rm get-pip.py
-wget https://bootstrap.pypa.io/get-pip.py -o get-pip3.py
+echo "[*] Downloading get-pip.py for python3"
+echo "[*] wget https://bootstrap.pypa.io/get-pip.py -o get-pip3.py"
+wget https://bootstrap.pypa.io/get-pip.py -O get-pip3.py
+echo "[*] python3 get-pip3.py"
 python3 get-pip3.py
+
+echo "[*] Cleaning Trash 0_0"
+echo "[*] rm get-pip.py"
+echo "[*] rm get-pip3.py"
+rm get-pip.py
 rm get-pip3.py
 
+echo "[*] [*] Checking pip version"
+echo "[*] pip --version"
+pip --version
+
+echo "[*] Checking pip2 version"
+echo "[*] pip2 --version"
+pip2 --version
+
+echo "[*] Checking pip3 version"
+echo "[*] pip3 --version"
+pip3 --version

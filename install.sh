43c3a6663a0b1dffb230be235d57ef10c5528f76
Abhiak99 <78052345+Abhiak99@users.noreply.github.com>
2022-01-16 19:52:57 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,8 @@ fi
 
 cp /usr/bin/python2 /usr/bin/python 
 wget https://bootstrap.pypa.io/pip/2.7/get-pip.py
-rm get-pip.py
 python get-pip.py
+rm get-pip.py
 wget https://bootstrap.pypa.io/get-pip.py -o get-pip3.py
 python3 get-pip3.py
 rm get-pip3.py
